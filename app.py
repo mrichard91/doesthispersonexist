66563fcae585286395e763ce83f1e5e93794baee
matt.richard@gmail.com <matt.richard@gmail.com>
2022-05-20 09:34:01 -0400

    add multiple url scanning

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, List
 from fastapi import FastAPI
 from pydantic import BaseModel
 import base64
 import aiohttp
+import asyncio
 
 from gan_detector import scan_data
 import json
@@ -16,14 +17,45 @@ class ScanResponse(BaseModel):
     error: Optional[str]
     results: Optional[Dict[str, Any]]
 
+class ScanMultipleRequest(BaseModel):
+    url: List[str]
+
+class ScanMultipleResponse(BaseModel):
+    status: str
+    error: Optional[List[str]]
+    results: Optional[Dict[str, Any]]
+
 app = FastAPI()
 
-async def fetch_url(url: str):
-    async with aiohttp.ClientSession() as session:
+async def fetch_url(url: str, session: aiohttp.ClientSession = None):
+    if session:
         r = await session.get(url)
         if r.status == 200:
             data = await r.content.read()
             return data
+    else:
+        async with aiohttp.ClientSession() as session:
+            r = await session.get(url)
+            if r.status == 200:
+                data = await r.content.read()
+                return data
+
+@app.post('/scan_urls', response_model=ScanResponse)
+async def url_scans(scan_request: ScanRequest) -> ScanResponse:
+    urls = scan_request.urls
+    async with aiohttp.ClientSession() as session:
+
+        async def fetch_scan(url):
+            data = await fetch_url(url, session)
+            return scan_data(data)
+
+        futures = [fetch_scan(url) for url in urls]
+        results = await asyncio.gather(*futures)
+    final_results = {
+        url:result
+        for url, result in zip(urls, results)
+    }
+    return {"status": "ok", "results": final_results}
 
 @app.post('/scan_url', response_model=ScanResponse)
 async def url_scan(scan_request: ScanRequest) -> ScanResponse:
